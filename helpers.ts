c3afee4399dfb006efd6a8bfe1c12452baac98f3
serkanilac <166456258+serkanilac@users.noreply.github.com>
2026-03-12 17:11:46 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T14:11:41.691Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-12T14:11:48.171Z
+Commit: Resolve null pointer exception in user service
