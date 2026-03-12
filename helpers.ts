7044574169c12dcae1629435fb5a543a812c0f6d
serkanilac <166456258+serkanilac@users.noreply.github.com>
2026-03-12 17:11:40 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T14:11:30.965Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-12T14:11:41.691Z
+Commit: Resolve timezone issues in date handling
