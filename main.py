c3878e1e5e8c9da6087eabe0eb1a8eb3bade2ed8
serkanilac <166456258+serkanilac@users.noreply.github.com>
2026-03-12 17:12:24 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T14:12:12.192Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-12T14:12:24.421Z
+Commit: Fix broken links in documentation
