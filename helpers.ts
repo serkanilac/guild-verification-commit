dfb32e15278f8059b43a6652a7dd13c6f59deedc
serkanilac <166456258+serkanilac@users.noreply.github.com>
2026-03-12 17:11:29 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T14:02:27.504Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-12T14:11:30.965Z
+Commit: Patch API endpoint error handling
